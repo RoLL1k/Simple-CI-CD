be79d47d516fc5c83f408f02e4f4a1382601e576
RoLL1K <>
2021-09-24 16:25:23 +0300

    Added template rendering

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
+
 
 @app.route("/")
 def index():
-	return "Hello, world!"
+	return render_template('index.html', name='Dima')
+
 
 if __name__ == "__main__":
 	app.run()
-
